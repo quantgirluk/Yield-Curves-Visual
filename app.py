073f19fa1dd9124b8dd9f2c6e2ffd301fd5e8b13
Quant Girl <quantgirl.uk@gmail.com>
2024-05-06 19:50:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -326,4 +326,4 @@ def toggle_modal(n1, n2, is_open):
 ####################################
 if __name__ == '__main__':
     server = app.server
-app.run_server(debug=True)
+app.run_server(debug=True, port=10000)
