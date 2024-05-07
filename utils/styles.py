e1e34b3f3e3364a91d6224b0c48c4bc28b8ede03
Quant Girl <quantgirl.uk@gmail.com>
2024-05-07 14:13:18 +0100

    Update styles.py

--- utils/styles.py
+++ utils/styles.py
@@ -1,13 +1,13 @@
 CONTENT_STYLE = {
     # "margin-left": "3rem",
     # "margin-right": "3rem",
-    "width": "85%",
+    "width": "90%",
     # "padding": "2rem 2rem 2rem 2rem",
 }
 
 CONTENT_INTRO = {
     # "margin-left": "3rem",
     # "margin-right": "3rem",
-    "width": "70%",
+    "width": "75%",
     "padding": "2rem 2rem 2rem 2rem",
 }
